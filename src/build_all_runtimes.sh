iamaleks/Cloudflare-Dynamic-DNS

commit bdee5a23ca43b1d67b932f1d013af200585736fe

Added parameters to the build file.
Added SelfContained, PublishSingleFile, and PublishTrimmed to Dotnet build
file. All values where set to true.

src/CloudflareDynamicDNS.csproj
@@ -3,6 +3,9 @@
   <PropertyGroup>
     <OutputType>Exe</OutputType>
     <TargetFramework>netcoreapp3.1</TargetFramework>
+    <SelfContained>True</SelfContained>
+    <PublishSingleFile>True</PublishSingleFile>
+    <PublishTrimmed>True</PublishTrimmed>
   </PropertyGroup>
 
     <ItemGroup>

commit cf06175aa3fe8582d4124827c925c5e91dd0f88d

Fix bug that affects build and Docker container
A bug was introduced by commit https://github.com/iamaleks/Cloudflare-Dynamic-DNS/commit/bdee5a23ca43b1d67b932f1d013af200585736fe.

When running a build command (dotnet publish -c Release -o out) and error pops up saying
'error NETSDK1031: It is not supported to build or publish a self-contained application without specifying a RuntimeIdentifier.
Please either specify a RuntimeIdentifier or set SelfContained to false.'.

This was fixed by removing the paramters added to the Dotnet build file and moving them to the build script.

src/CloudflareDynamicDNS.csproj
@@ -3,9 +3,6 @@
   <PropertyGroup>
     <OutputType>Exe</OutputType>
     <TargetFramework>netcoreapp3.1</TargetFramework>
-    <SelfContained>True</SelfContained>
-    <PublishSingleFile>True</PublishSingleFile>
-    <PublishTrimmed>True</PublishTrimmed>
   </PropertyGroup>
 
     <ItemGroup>

src/build_all_runtimes.sh
@@ -5,5 +5,5 @@ declare -a runtimes=("win-x64" "win-x86" "win-arm" "win-arm64" "win7-x64" "win7-
 for i in "${runtimes[@]}"
 do
    echo "Bulding for runtime: $i"
-   dotnet publish -r $i -c Release
+   dotnet publish -r $i -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true --self-contained
 done